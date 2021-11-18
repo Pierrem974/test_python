a83037295e962f15a1e1a292f5cdd800aa7f77e7
Pierrem974 <pierre.maillot@epitech.eu>
2021-11-18 15:32:54 +0400

    exo 2

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 
-print("Success")
+fruit = "apple"
+quantity = 5
+pie_crust = "empty"
+
+isOvenOn = True
+
+print(f"You  have {quantity} {fruit} and the pie  crust  is {pie_crust} , is the  oven on? {isOvenOn}")
